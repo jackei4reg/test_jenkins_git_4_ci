350f1b9fe7afedec415919dc483b8cae3a4f6c31
Jackei <jackei4reg@gmail.com>
2012-04-14 00:37:13 +0800

    Update test_python2.py

--- test_python2.py
+++ test_python2.py
@@ -1,1 +1,9 @@
-assert 2==2
+from test_python import *
+import unittest
+
+class first_test(unittest.TestCase):
+  def test_first_case(self):
+    self.assertEqual(2, twonumberadd(1, 1))
+    
+  def test_second_case(self):
+    self.assertEqual(3, twonumberadd(1, 1))
